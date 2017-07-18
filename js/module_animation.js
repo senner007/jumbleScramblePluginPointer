0c2429345f64219326438905708fbb0d5b9b54ae
senner007 <nielshtg@hotmail.com>
2017-07-18 04:40:09 +0200

    refactor animateBack function

--- js/module_animation.js
+++ js/module_animation.js
@@ -5,58 +5,38 @@ export {
 // ES6 MODULE IMPORT/EXPORT
 ////////////////////////////
 
-
-
-
 function transToZero(elt, speed) {
 
   var thisInst = this;
-if (speed == undefined) {
-  var speed = '250ms ease'
-}
+  if (speed == undefined) {
+    var speed = '250ms ease'
+  }
 
   window.getComputedStyle(elt)[thisInst.transformPrefix] // needed to apply the transition style dynamically
-
   elt.style[thisInst.transitionPrefix] = speed;
-
   elt.style[thisInst.transformPrefix] = thisInst.ifGpu // translateZ doesn't work for ie9
 
 };
 
 function animateBack(elt, o, thisInst) {
 
-
-
   var eltMarginLeft = o.isVertical ? 0 : elt.completeWidth - elt.offsetWidth; // set margin for horizontal
   if (elt.hasCrossed) {
-
-    var instMovesTo = thisInst.adjInst;
-
-    var adjEltBefore = instMovesTo.elts[thisInst.added.n - 1];
+    var adjEltBefore = thisInst.adjInst.elts[thisInst.added.n - 1];
     if (o.isVertical) {
-
-      var adjacentDir = instMovesTo.divOffset.left - thisInst.divOffset.left;
-      var animateToPos = thisInst.added.n > 0 ? adjEltBefore.pos.top + adjEltBefore.completeHeight : 0;
-
-      var thisLeft = adjacentDir,
-        thisTop = animateToPos,
-        thisX = elt.currentPos.left - adjacentDir,
-        thisY = elt.currentPos.top - animateToPos;
+      var thisLeft = thisInst.adjInst.divOffset.left - thisInst.divOffset.left;
+      var thisTop = thisInst.added.n > 0 ? adjEltBefore.pos.top + adjEltBefore.completeHeight : 0;
     } else {
-
-      var adjacentDir = instMovesTo.divOffset.top - thisInst.divOffset.top;
-      var animateToPos = thisInst.added.n > 0 ? adjEltBefore.pos.left + adjEltBefore.completeWidth : 0;
-
-      var thisLeft = animateToPos,
-        thisTop = adjacentDir,
-        thisX = elt.currentPos.left - animateToPos,
-        thisY = elt.currentPos.top - adjacentDir;
+      var thisTop = thisInst.adjInst.divOffset.top - thisInst.divOffset.top;
+      var thisLeft = thisInst.added.n > 0 ? adjEltBefore.pos.left + adjEltBefore.completeWidth : 0;
     }
+    var thisX = elt.currentPos.left - thisLeft,
+      thisY = elt.currentPos.top - thisTop;
   } else {
     var thisLeft = elt.pos.left,
       thisTop = elt.pos.top,
-      thisX = elt.currentPos.left - elt.pos.left,
-      thisY = elt.currentPos.top - elt.pos.top;
+      thisX = elt.currentPos.left - thisLeft,
+      thisY = elt.currentPos.top - thisTop;
   }
 
   elt.style.left = thisLeft + 'px'
