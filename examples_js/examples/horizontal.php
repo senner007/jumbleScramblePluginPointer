02b2e02d5befb36b7801bc449f87f377f3bad7ef
senner007 <nielshtg@hotmail.com>
2017-07-28 04:39:18 +0200

    examplify css counters

--- examples_js/examples/horizontal.php
+++ examples_js/examples/horizontal.php
@@ -1,29 +1,29 @@
 <?php
 echo "<div class='jMyPuzzle' id='jMyPuzzleId5'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>1st Element<button class='liButton'>Shuffle elements</button><span>This text is in span<span></li>
-    <li class='listItem-horizontal'>2nd Element</li>
-    <li class='listItem-horizontal'>3rd Element</li>
-    <li class='listItem-horizontal'>4th Element</li>
-    <li class='listItem-horizontal'>5th Element</li>
+    <li class='listItem-horizontal'>Element<button class='liButton'>Shuffle elements</button><span>This text is in span<span></li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
   </ul>
 </div>
 <div class='jMyPuzzle' id='jMyPuzzleId6'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>6th Element</li>
-    <li class='listItem-horizontal'>7th Element</li>
-    <li class='listItem-horizontal'>8th Element</li>
-    <li class='listItem-horizontal'>9th Element</li>
-    <li class='listItem-horizontal'>10th Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+
   </ul>
 </div>
 <div class='jMyPuzzle' id='jMyPuzzleId7'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>6th Element</li>
-    <li class='listItem-horizontal'>7th Element</li>
-    <li class='listItem-horizontal'>8th Element</li>
-    <li class='listItem-horizontal'>9th Element</li>
-    <li class='listItem-horizontal'>10th Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'>Element</li>
+
   </ul>
 </div>"
 ?>
