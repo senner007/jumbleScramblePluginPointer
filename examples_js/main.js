7b242e1daa6bedd05180a4837e953c6166a2f121
senner007 <nielshtg@hotmail.com>
2017-08-20 04:25:03 +0200

    clean up main js

--- examples_js/main.js
+++ examples_js/main.js
@@ -1,33 +1,36 @@
-import {vertical} from "./examples/vertical.js"
-import {horizontal} from "./examples/horizontal.js"
-import {pageSetup} from "./pageSetup.js"
+import {
+  vertical
+} from "./examples/vertical.js"
+import {
+  horizontal
+} from "./examples/horizontal.js"
+import {
+  pageSetup
+} from "./pageSetup.js"
 import JumbleScramble from "../plugin_js/module_main.js";
 // ES6 MODULE IMPORT/EXPORT
 ////////////////////////////
+$(document).ready(function() {
 
-$('.container').hide(); // cannot be set from css ???
+  $('.container').hide(); // cannot be set from css ???
 
-var exampleObject = {
-  vertical : vertical,
-  horizontal : horizontal
+  var exampleObject = {
+    vertical: vertical,
+    horizontal: horizontal
+  }
 
-}
-
-var path = window.location.pathname.split('/');
-if (path[2].length == 0) {
+  var path = window.location.pathname.split('/');
+  if (path[2].length == 0) {
     $('.bodyButton').show().on('click', function(e) {
       console.log('hello')
       exampleObject[e.target.textContent]();
     });
-}
-else if (path[2] == 'vertical' ){
+  } else if (path[2] == 'vertical') {
     exampleObject.vertical();
 
-
-}
-else if (path[2] == 'horizontal' ) {
-  exampleObject.horizontal();
-}
-
-
-pageSetup();
+  } else if (path[2] == 'horizontal') {
+    exampleObject.horizontal();
+  }
+  
+  pageSetup();
+});
