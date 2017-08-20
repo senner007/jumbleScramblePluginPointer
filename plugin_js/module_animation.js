871d95b318559a6789af0cdaeb1fb700da82b2dd
senner007 <nielshtg@hotmail.com>
2017-08-20 04:28:00 +0200

    refactor animation module
    ...account for individual vertical width

--- plugin_js/module_animation.js
+++ plugin_js/module_animation.js
@@ -1,3 +1,7 @@
+import {
+  setEvents
+} from "./module_utils.js"
+
 export {
   _animateBack,
   _transToZero,
@@ -7,58 +11,66 @@ export {
 ////////////////////////////
 
 function _transToZero(elt, thisInst, speed) {
-
-
-  if (speed == undefined) {
-    var speed = '250ms ease'
-  }
+  if (speed == undefined) { var speed = '250ms ease' }
   window.getComputedStyle(elt)[thisInst.transformPrefix] // needed to apply the transition style dynamically
   elt.style[thisInst.transitionPrefix] = speed;
   elt.style[thisInst.transformPrefix] = thisInst.ifGpu // translateZ doesn't work for ie9
-
+  // Refactor into cssText line !!!!!!!!!!!!!!!
 };
 
 function _animateBack(elt, thisInst) {
   var o = thisInst.options;
 
-  var eltMarginLeft = o.isVertical ? 0 : elt.completeWidth - elt.offsetWidth; // set margin for horizontal
-  if (thisInst.crossFlag) {
+  var eltMarginLeft = o.isVertical ? 0 : elt.props.completeWidth - elt.offsetWidth; // set margin for horizontal
+  if (thisInst.crossFlag && thisInst.newInst) {
+
     if (o.isVertical) {
-      var thisTop = thisInst.added.pos.top;
+      var thisTop = thisInst.added.props.pos.top;
       var thisLeft = thisInst.crossDistance(thisInst, thisInst.newInst);
+      var diff = Math.abs ( thisInst.props.divWidth - thisInst.newInst.props.divWidth)
+
+
     } else {
       var thisTop = thisInst.crossDistance(thisInst, thisInst.newInst);
-      var thisLeft = thisInst.added.pos.left;
+      var thisLeft = thisInst.added.props.pos.left;
+
     }
   } else {
-    var thisTop = elt.pos.top,
-        thisLeft = elt.pos.left;
+    var thisTop = elt.props.pos.top,
+        thisLeft = elt.props.pos.left;
   }
 
+// Refactor into cssText line !!!!!!!!!!!!!!!
+console.log(diff)
+  if (diff > 2) {
+    elt.style.width = thisInst.newInst.props.divWidth + 'px'
+    elt.style[thisInst.transitionPrefix] = 'width 200ms';
+  }
 
   elt.style.top = thisTop + 'px';
   elt.style.left = thisLeft + 'px';
-  elt.style[thisInst.transformPrefix] = 'translate3d(' + ((elt.currentPos.left - thisLeft) - eltMarginLeft) + 'px,' + (elt.currentPos.top - thisTop) + 'px,0px)';
+  elt.style[thisInst.transformPrefix] = 'translate3d(' + ((elt.props.currentPos.left - thisLeft) - eltMarginLeft) + 'px,' + (elt.props.currentPos.top - thisTop) + 'px,0px)';
 
 };
 
-function _scaleElems(elems, thisInst) { // should be in animation module, and not on the prototype
+function _scaleElems(elems, thisInst) {
  if (elems == undefined) { return; }
-  var elems = elems, // elems is an array of elements to scale in after they have been added
+ var elems = elems, // elems is an array of elements to scale in after they have been added
     thisInst = thisInst;
 
   scaleElems('off');
 
   if (thisInst.transSupport && elems.length != 0) { // transition elements  but only if if there are any
 
-     setTimeout(function() { scaleElems('on'); }, 1);
+     setTimeout(function() {scaleElems('on');}, 1);
       // setTimeout is need because transform properties need time to be set.
   }
-
   function scaleElems(trigger) {
     for (var i = 0; i < elems.length; i++) {
+      // Refactor into cssText line !!!!!!!!!!!!!!!
       elems[i].style[thisInst.transitionPrefix] = trigger == 'on' ? '500ms' : '0ms';
       elems[i].style[thisInst.transformPrefix] = trigger == 'on' ? 'scale(1,1)' : 'scale(0,0)';
     }
   }
+  return elems.length
 };
