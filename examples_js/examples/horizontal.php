473f38256459a3383fbef2027bbe83cc8b8aa788
senner007 <nielshtg@hotmail.com>
2017-08-20 04:24:36 +0200

    insert special span

--- examples_js/examples/horizontal.php
+++ examples_js/examples/horizontal.php
@@ -1,28 +1,28 @@
 <?php
 echo "<div class='jMyPuzzle' id='jMyPuzzleId5'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>Element<button class='liButton'>Shuffle elements</button><span>This text is in span<span></li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
   </ul>
 </div>
 <div class='jMyPuzzle' id='jMyPuzzleId6'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
 
   </ul>
 </div>
 <div class='jMyPuzzle' id='jMyPuzzleId7'>
   <ul touch-action='none' class='splitList-horizontal'>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
-    <li class='listItem-horizontal'>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
+    <li class='listItem-horizontal'><span class='special'></span>Element</li>
 
   </ul>
 </div>"
